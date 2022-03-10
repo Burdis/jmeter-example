54526beabe54c47b58bbec1d98b33415c9d72a73
Andy Burdis <andy.burdis@yahoo.com>
2022-03-10 15:14:28 +0000

    Added express app, basic jmx file and run script

---
+++ runandreport.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#remove existing report csv and results folder (assuming this script is in the same directory as the results dir and report.csv)
+rm -rf report.csv
+rm -rf results
+
+
+#move into jmeter location before running tests (filepaths need to be changed per setup)
+cd ../apache-jmeter-5.4.1/bin/
+
+#run JMeter and generate a report (filepaths need to be changed per setup)
+./jmeter.sh -n -t ../../jmeter-example/performance.jmx -l ../../jmeter-example/report.csv -e -o ../../jmeter-example/results
